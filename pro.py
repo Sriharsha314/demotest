9c07223c354bdfac3040c796aa54ec330dafd20f
nagaraj90199 <kn733478@gmail.com>
2026-01-17 00:06:02 +0530

    save

---
+++ pro.py
@@ -0,0 +1,8 @@
+def cal_samlest(a,b,c):
+    return min(a,b,c)
+def cal_largest(a,b,c):
+    return max(a,b,c)
+
+if __name__ =="__main__":
+    print("Smallest:",cal_samlest(10,20,5))
+    print("Largest:",cal_largest(10,20,5))
---
+++ test_pro.py
@@ -0,0 +1,6 @@
+from pro import cal_samlest, cal_largest
+def test_cal_samlesta():
+    assert cal_samlest(10,20,5) == 5
+
+def test_cal_largesta():
+    assert cal_largest(10,20,5) == 20
